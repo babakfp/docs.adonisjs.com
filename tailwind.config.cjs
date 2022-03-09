4e9d902b355dcaefc94ae5836d547672352bcaf7
babakfp <44144724+babakfp@users.noreply.github.com>
2022-03-08 16:30:25 -0800

    Added tw custom colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,20 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
 	content: ['./resources/**/*.{edge,js}'],
   theme: {
-    extend: {},
+    extend: {
+			colors: {
+				gray: colors.neutral,
+				brand: {
+					DEFAULT: '#5A45FF',
+					faded: '#AFA7EF',
+				},
+				accent: {
+					DEFAULT: '#02E2FF',
+				},
+			},
+		},
   },
   plugins: [],
 }
